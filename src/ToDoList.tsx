755b2aa9bee78be05e16eed0e5eac5b52cae1865
Nadyushka <12nadya11@gmail.com>
2022-11-28 21:40:01 +0300

    finish monday lesson 4

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,8 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {FilterValueType, TaskType} from "./App";
+import './App.css';
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 type ToDolIstPropsType = {
     title: string
@@ -7,6 +10,10 @@ type ToDolIstPropsType = {
     removeTask: (taskId: string) => void
     changeToDoListFilter: (nextFilterValue: FilterValueType) => void
     addTask: (newTitleTask: string) => void
+    changeTaskStatus: (taskId: string, isDone: boolean) => void
+    filter: FilterValueType
+    error: boolean
+    setError: (value: boolean) => void
 }
 
 const ToDoList = (props: ToDolIstPropsType) => {
@@ -19,6 +26,7 @@ const ToDoList = (props: ToDolIstPropsType) => {
     }
 
     const setLocalTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        error && props.setError(false)
         setNewTask(event.currentTarget.value)
     }
 
@@ -28,40 +36,57 @@ const ToDoList = (props: ToDolIstPropsType) => {
         }
     }
 
-    const taskElements = props.tasks.map((task: TaskType) => {
-            const removeTask = () => props.removeTask(task.id)
-            return <li key={task.id}>
-                <input type="checkbox" checked={task.isDone}/>
-                <span>{task.title}</span>
-                <button onClick={removeTask}>x</button>
-            </li>
-        }
-    )
+    const taskElements = props.tasks.length !== 0 ? props.tasks.map((task: TaskType) => {
+
+        const removeTask = () => props.removeTask(task.id)
+        const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => props.changeTaskStatus(task.id, e.currentTarget.checked)
+
+        return <li key={task.id} className={task.isDone === true ? 'task-done' : ''}>
+            <input type="checkbox"
+                   checked={task.isDone}
+                   onChange={changeTaskStatus}
+            />
+            <span>{task.title}</span>
+            <button onClick={removeTask}>x</button>
+        </li>
+    }) : <li>Add your list</li>;
 
     const OnClickHandlerCreator = (filter: FilterValueType) => () => props.changeToDoListFilter(filter)
 
 
     return (
         <div>
+
             <h3>{props.title}</h3>
             <div>
                 <input
                     value={newTask}
                     onKeyDown={onEnterAddTask}
-                    onChange={setLocalTitle}/>
+                    onChange={setLocalTitle}
+                    className={props.error == true ? 'input-error' : ''}
+                />
+
                 <button onClick={addTask}>+</button>
+                {props.error && <div style={{color:'red', fontWeight:600}}>Add correct task</div>}
             </div>
             <ul>
                 {taskElements}
+
             </ul>
             <div>
-                <button onClick={OnClickHandlerCreator('all')}>All</button>
-                <button onClick={OnClickHandlerCreator('active')}>Active</button>
-                <button onClick={OnClickHandlerCreator('completed')}>Completed</button>
+                <button className={props.filter === "all" ? 'btn-color' : ''}
+                        onClick={OnClickHandlerCreator('all')}>All
+                </button>
+                <button className={props.filter === "active" ? 'btn-color' : ''}
+                        onClick={OnClickHandlerCreator('active')}>Active
+                </button>
+                <button className={props.filter === "completed" ? 'btn-color' : ''}
+                        onClick={OnClickHandlerCreator('completed')}>Completed
+                </button>
             </div>
 
         </div>
-    );
+    )
 };
 
 export default ToDoList;
