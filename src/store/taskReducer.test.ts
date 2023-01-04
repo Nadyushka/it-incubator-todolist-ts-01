29e90a8e0b52727649b86f882c0f47f83d2d291b
Nadyushka <12nadya11@gmail.com>
2023-01-04 21:10:13 +0300

    lesson 9 finish

--- src/store/taskReducer.test.ts
+++ src/store/taskReducer.test.ts
@@ -84,3 +84,24 @@ test('correct task title should be changed ', () => {
 
 })
 
+test('correct task status should be changed ', () => {
+    const startState: TaskStateType = {
+        "todolistId1": [
+            {id: "1", title: "CSS", isDone: false},
+            {id: "2", title: "JS", isDone: true},
+            {id: "3", title: "React", isDone: false}
+        ],
+        "todolistId2": [
+            {id: "1", title: "bread", isDone: false},
+            {id: "2", title: "milk", isDone: true},
+            {id: "3", title: "tea", isDone: false}
+        ]
+    }
+
+    const endState = taskReducer(startState, ChangeTaskStatusAC('todolistId2', '3', true))
+
+
+    expect(startState['todolistId2'][2]['isDone']).toBeFalsy()
+    expect(endState['todolistId2'][2]['isDone']).toBeTruthy()
+
+})
